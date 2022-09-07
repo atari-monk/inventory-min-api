e828c579d69326892907d7cd1081be97ddd99ee0
krzm <kmazanek@gmail.com>
2022-09-07 21:50:58 +0200

    Add 6 props to item, add db option

--- Inventory.Min.Api/Dto/ItemCreateDto.cs
+++ Inventory.Min.Api/Dto/ItemCreateDto.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Inventory.Min.Data;
 using ModelHelper;
 
 namespace Inventory.Min.Api.Dto;
@@ -12,4 +14,20 @@ public class ItemCreateDto
 
     [MaxLength(DescriptionMax)]
 	public string? Description { get; set; }
+
+    public int? InitialCount { get; set; }
+
+    public int? CurrentCount { get; set; }
+
+    [ForeignKey(nameof(Category))]
+	public int? CategoryId { get; set; }
+
+	[Column(TypeName = Datetime2Name)]
+	public DateTime? PurchaseDate { get; set; }
+
+    [ForeignKey(nameof(Currency))]
+	public int? CurrencyId { get; set; }
+
+    [Column(TypeName = "decimal(5, 2)")]
+    public decimal? PurchasePrice { get; set; }
 }
